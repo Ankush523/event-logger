c68ec7b84636402a676bcc54628d0ed34ed8fa4b
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-09-21 18:42:51 +0530

    axios added

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const Model = require('../models/model');
 
 const router = express.Router()
 
-//Post Method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        sender: req.body.sender,
+        sum: req.body.sum
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
-//Get all Method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -7,7 +7,18 @@ async function main() {
     const contractAddr = "0xc21d985f43520741CBB25C58EbCC78efDAB49E86";
     const provider = new ethers.providers.WebSocketProvider("wss://eth-goerli.g.alchemy.com/v2/CxL97XWU2bhy83jkLmdcBZjCSyJwlkVp");
     const addContract = new ethers.Contract(contractAddr,addABI,provider)
-    addContract.on("Addition",(owner,sum) => {
-        
-    })
+    addContract.on('Addition', (owner,sum) => {
+        var data = sum.toString();
+        axios.post('http://localhost:3000/api/post', {
+            sender: owner,
+            sum: data
+          })
+          .then((response) => {
+            console.log(response);
+          }, (error) => {
+            console.log(error);
+          });
+        console.log('Addition Event: ', owner,sum);
+    }
+    );
 }
